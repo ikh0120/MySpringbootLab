3058cf6d16b85e5a0b34809dcc0c440c8dbb4ff2
ikh0120 <lkh50635164@gmail.com>
2025-05-12 21:13:27 +0900

    BookController에서 BookRepository를 명시적으로 import(import com.rookies3.myspringbootlab.repository.BookRepository;) 했을 땐 Cannot resolve symbol 'BookRepository'라는 Error가 발생했지만 와일드카드로 import(import com.rookies3.myspringbootlab.repository.*;)를 진행했을 땐 Error 해결

--- src/main/java/com/rookies3/myspringbootlab/controller/BookController.java
+++ src/main/java/com/rookies3/myspringbootlab/controller/BookController.java
@@ -1,17 +1,21 @@
 package com.rookies3.myspringbootlab.controller;
 
 import com.rookies3.myspringbootlab.entity.Book;
+
+import com.rookies3.myspringbootlab.repository.*;
+//import com.rookies3.myspringbootlab.repository.BookRepository;
+
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rookies3.myspringbootlab.repository.BookRepository;
-
-@RestController //@ResponseBody + Controller
-@RequiredArgsConstructor //Lombok이 final 필드나 @NonNull 필드 기반으로 생성자 자동 생성
-@RequestMapping("/api/books") //이 컨트롤러의 모든 메서드는 "/api/books" 경로를 기본으로 가짐
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/books")
 public class BookController {
 
     private final BookRepository bookRepository;
@@ -20,6 +24,7 @@ public class BookController {
     public Book create(@RequestBody Book book){
         return bookRepository.save(book);
     }
+
 
 
 }
