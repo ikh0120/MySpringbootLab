b0f973da50e23f09d75510313445201861b7157d
ikh0120 <lkh50635164@gmail.com>
2025-05-19 10:34:36 +0900

    Entity, Repository, DTO, Test finish

--- src/main/java/com/rookies3/myspringbootlab/repository/BookDetailRepository.java
+++ src/main/java/com/rookies3/myspringbootlab/repository/BookDetailRepository.java
@@ -3,6 +3,7 @@ package com.rookies3.myspringbootlab.repository;
 import com.rookies3.myspringbootlab.entity.BookDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public interface BookDetailRepository extends JpaRepository<BookDetail, Long> {
     Optional<BookDetail> findByBookId(Long bookId);
 
     @Query("SELECT bd FROM BookDetail bd JOIN FETCH bd.book WHERE bd.id = :id")
-    Optional<BookDetail> findByIdWithBook(Long id);
+    Optional<BookDetail> findByIdWithBook(@Param("id") Long id);
 
     List<BookDetail> findByPublisher(String publisher);
 }
