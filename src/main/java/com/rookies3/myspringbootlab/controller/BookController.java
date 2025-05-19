ce7c7425ec799cf09c3ae02d07e4c70caffc4c5f
ikh0120 <lkh50635164@gmail.com>
2025-05-19 14:09:57 +0900

    Controller start

--- src/main/java/com/rookies3/myspringbootlab/controller/BookController.java
+++ src/main/java/com/rookies3/myspringbootlab/controller/BookController.java
@@ -1,100 +1,17 @@
 package com.rookies3.myspringbootlab.controller;
 
-import com.rookies3.myspringbootlab.controller.dto.BookDTO;
-import com.rookies3.myspringbootlab.service.BookService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import com.rookies3.myspringbootlab.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("api/books")
 @RequiredArgsConstructor
 public class BookController{
 
-    private final BookService bookService;
-
-    // 성공
-    @GetMapping
-    public ResponseEntity<List<BookDTO.BookResponse>> getAllBooks() {
-        List<BookDTO.BookResponse> allBooks = bookService.getAllBooks();
-//        return new ResponseEntity<>(allBooks, HttpStatus.OK);
-        return ResponseEntity.ok(allBooks);
-    }
-
-//    @GetMapping
-//    public List<BookDTO.BookResponse> getAllBooks() {
-//        return bookService.getAllBooks()
-//                .stream()
-//                .map(BookDTO.BookResponse::from)
-//                //.map(book -> BookDTO.BookResponse.from(book))
-//                .toList();
-//    }
-
-    //성공
-    @GetMapping("/id/{id}")
-    public ResponseEntity<BookDTO.BookResponse> getBookById(@PathVariable Long id){
-        return new ResponseEntity<>(
-                bookService.getBookById(id),
-                HttpStatus.OK
-        );
-    }
-
-
-
-    //성공
-    @GetMapping("/isbn/{isbn}")
-    public ResponseEntity<BookDTO.BookResponse> getBookByIsbn(@PathVariable String isbn) {
-        return new ResponseEntity<>(
-                bookService.getBookByIsbn(isbn),
-                HttpStatus.FOUND
-        );
-    }
-
-
-
-    //성공
-    @GetMapping("/author/{author}")
-    public ResponseEntity<List<BookDTO.BookResponse>> getBookByAuthor(@PathVariable String author) {
-        return new ResponseEntity<>(
-                bookService.getBookByAuthor(author),
-                HttpStatus.OK
-        );
-    }
-
-
-    //성공
-    @PostMapping
-    public ResponseEntity<BookDTO.BookResponse> createBook(
-            @Valid @RequestBody BookDTO.BookCreateRequest request) {
-        return new ResponseEntity<>(
-                bookService.createBook(request),
-                HttpStatus.CREATED
-        );
-    }
-
-
-    //성공
-    @PatchMapping("/{id}")
-    public ResponseEntity<BookDTO.BookResponse> updateBook(
-            @PathVariable Long id, @Valid @RequestBody BookDTO.BookUpdateRequest request) {
-        return new ResponseEntity<>(
-                bookService.updateBook(id, request),
-                HttpStatus.OK
-        );
-    }
-
-    //성공
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
-        bookService.deleteBook(id);
-        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-    }
-
-
+    private final BookService bookservice;
 
 
 }
