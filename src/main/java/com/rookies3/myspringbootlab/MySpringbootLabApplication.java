a96ddd05028a529a3c3ac3539cd15322484d947c
ikh0120 <lkh50635164@gmail.com>
2025-05-24 02:20:51 +0900

    update all

--- src/main/java/com/rookies3/myspringbootlab/MySpringbootLabApplication.java
+++ src/main/java/com/rookies3/myspringbootlab/MySpringbootLabApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MySpringBootLabApplication {
+public class MySpringbootLabApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootLabApplication.class, args);
+		SpringApplication.run(MySpringbootLabApplication.class, args);
 	}
 
 }
